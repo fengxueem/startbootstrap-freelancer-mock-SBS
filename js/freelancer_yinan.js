c180a2f5c3337587b363600f995d134da0add936
fengxueem <fengxueem@gmail.com>
2017-06-25 23:25:01 +0800

    Hot fix: close navbar menu only when it's visible

--- js/freelancer_yinan.js
+++ js/freelancer_yinan.js
@@ -1,6 +1,6 @@
 // Closes the Responsive Menu on Menu Item Click
 $('#yinan-navbar-collapse-1 ul li a').click(function(){ 
-	$('.navbar-toggle').click();
+	$('.navbar-toggle:visible').click();
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
